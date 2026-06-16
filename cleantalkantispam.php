9738a1d768c1a70c3f00fc3148ba16a07c4eaa7d
Viktor <Bazz@bk.ru>
2026-06-16 16:13:43 +0300

    Update cleantalkantispam.php

--- cleantalkantispam.php
+++ cleantalkantispam.php
@@ -34,7 +34,7 @@ class CleantalkAntispam extends Module
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
             'min' => '1.7.0.0',
-            'max' => '9.0.3',
+            'max' => _PS_VERSION_,
         ];
         $this->bootstrap = true;
 
