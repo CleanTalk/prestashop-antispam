36f9ad9a70c15a276b869f2fd6640dcd2b585d84
Glomberg <bazz@bk.ru>
2026-07-06 10:44:14 +0300

    Fix. Bot detector. Bot detector loading fixed.

--- cleantalkantispam.php
+++ cleantalkantispam.php
@@ -76,7 +76,7 @@ class CleantalkAntispam extends Module
     public function hookDisplayHeader()
     {
         if (Configuration::get('CLEANTALKANTISPAM_ENABLE_BOTDETECTOR')) {
-            return '<script src="https://fd.cleantalk.org/ct-bot-detector-wrapper.js"></script>';
+            return '<script src="https://fd.cleantalk.org/ct-bot-detector-wrapper.js" async="async"></script>';
         }
 
         return '';
